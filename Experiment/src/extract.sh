fbead8735b223c4d2edfb65637022e9241c60500
CodeNachos <rafael.nery14@gmail.com>
2025-03-26 01:08:14 +0100

    update extract script

--- Experiment/src/extract.sh
+++ Experiment/src/extract.sh
@@ -2,19 +2,53 @@
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+DATASET_PATH="$SRC_DIR/../res/datasets/"
 # source zip file name
-ZIP_FILE="$SRC_DIR/../res/datasets/mit_stimuli_scenes.zip"
+DATASET_FILE="$DATASET_PATH/mit_stimuli_scenes.zip"
+
+DATASET_URL="http://olivalab.mit.edu/MM/downloads/Scenes.zip"
 
 # categories list to be extracted
 FOLDER_LIST="$SRC_DIR/../res/categories.txt"
+
+OUTPUT_PATH="$SRC_DIR/../res/stimuli/original/"
+
+# Check if the categories list file exists
+if [[ ! -f "$FOLDER_LIST" ]]; then
+    echo "Error: Categories list '$FOLDER_LIST' not found!"
+    exit 1
+fi
+
+# Check if the dataset zip file exists
+if [[ ! -f "$DATASET_FILE" ]]; then
+    echo -e "Dataset file not found! Downloading from $DATASET_URL...\n"
+    
+    # Ensure the dataset directory exists
+    mkdir -p "$DATASET_PATH"
+    
+    # Download the zip file using wget
+    wget -O "$DATASET_FILE" "$DATASET_URL"
+    
+    # Check if the download was successful
+    if [[ ! -f "$DATASET_FILE" ]]; then
+        echo "Error: Failed to download the dataset!"
+        exit 2
+    else
+        echo "Dataset successfully downloaded!"
+    fi
+else
+    echo "Dataset file found!"
+fi
+
+echo -e "\nExtracting files from category list...\n"
 
 # loop through each line in the text file and extract category folder
 while IFS= read -r folder; do
     if [[ -n "$folder" ]]; then 
         echo "Extracting $folder..."
-        mkdir -p "$SRC_DIR/../res/stimuli/original/"
-        unzip -q -n "$ZIP_FILE" "$folder/*.jpg" -d "$SRC_DIR/../res/stimuli/original/"
+        mkdir -p "$OUTPUT_PATH"
+        unzip -q -n "$DATASET_FILE" "$folder/*.jpg" -d "$OUTPUT_PATH"
     fi
 done < "$FOLDER_LIST"
 
-echo "Extraction completed."
+echo -e "\nExtraction completed."
