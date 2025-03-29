12c32ad6c4848a40f87e182d50c4cf145736299b
CodeNachos <rafael.nery14@gmail.com>
2025-03-29 13:48:34 +0100

    flow and experiment protocol implementation

--- Experiment/src/experiment.py
+++ Experiment/src/experiment.py
@@ -5,9 +5,11 @@ import logging
 import argparse
 import stimgrab
 import numpy as np
+from time import time
 from typing import Dict
 from pathlib import Path
 from enum import IntEnum
+from itertools import chain
 from strenum import StrEnum
 from collections import defaultdict
 from psychopy import visual, core, event
@@ -22,14 +24,14 @@ NB_MODALITIES = 2
 
 class _CacheKeys(StrEnum):
     SUBJECTS = 'total_subjects'
-    CATIMGS = 'images_per_category',
+    CATIMGS = 'images_per_category'
     CATEGORIES = 'category_list'
     REDFILTER = 'red_filter'
-    CYANFILTER = 'cyan_filter',
+    CYANFILTER = 'cyan_filter'
     USERANDSEED = 'use_random_seed'
-    RANDSEED = 'random_seed',
+    RANDSEED = 'random_seed'
 
-WIN = visual.Window(monitor="testMonitor",  units="pix", fullscr=True, allowStencil=True)
+WIN = visual.Window(monitor="testMonitor",  units="pix", fullscr=False, allowStencil=True)
 
 _TRIAL_INSTRUCTION = "" \
 "Please press the space bar as soon as you see an image appear trough the mask."
@@ -49,7 +51,7 @@ _DEFAULTS = {
     _CacheKeys.CATEGORIES   : (_RELATIVE_PATH / "../res/expdata/categories.txt").resolve(),
     _CacheKeys.REDFILTER    : [-.75, -.1, -.1],
     _CacheKeys.CYANFILTER   : [-.1, -.7, -.7],
-    _CacheKeys.USERANDSEED  : True,
+    _CacheKeys.USERANDSEED  : False,
     _CacheKeys.RANDSEED     : 42
 }
 
@@ -116,7 +118,7 @@ parser.add_argument(
 )
 parser.add_argument(
     '--use-seed',
-    action='set-false',
+    action='store_true',
     help="If given it won't use a constant random seed"
 )
 parser.add_argument(
@@ -132,7 +134,7 @@ _categories:Path = _DEFAULTS[_CacheKeys.CATEGORIES]
 _red_filter = _DEFAULTS[_CacheKeys.REDFILTER]
 _cyan_filter = _DEFAULTS[_CacheKeys.CYANFILTER]
 _random_seed = _DEFAULTS[_CacheKeys.RANDSEED]
-_use_random_seed = _DEFAULTS[_CacheKeys.USERANDSEED]
+_use_seed = _DEFAULTS[_CacheKeys.USERANDSEED]
 
 # UTILITY ======================================================================
 
@@ -140,24 +142,23 @@ _use_random_seed = _DEFAULTS[_CacheKeys.USERANDSEED]
 
 def _handle_args():
     args = parser.parse_args()
-    
     for key, value in vars(args).items():
         if value is not None:
             if key == "reset":
                 if value: _load_defaults()
             elif key == "set_subjects":
                 _update_cache(_CacheKeys.SUBJECTS, value)
-            elif key == "images-per-category":
+            elif key == "images_per_category":
                 _update_cache(_CacheKeys.CATIMGS, value)
             elif key == "categories":
                 _update_cache(_CacheKeys.CATEGORIES, value)
-            elif key == "red-filter":
+            elif key == "red_filter":
                 _update_cache(_CacheKeys.REDFILTER, value)
-            elif key == "cyan-filter":
+            elif key == "cyan_filter":
                 _update_cache(_CacheKeys.CYANFILTER, value)
             elif key == "seed":
                 _update_cache(_CacheKeys.RANDSEED, value)
-            elif key == 'use-seed':
+            elif key == 'use_seed':
                 _update_cache(_CacheKeys.USERANDSEED, value)
             elif key == "verbose":
                 if value: 
@@ -217,7 +218,7 @@ def _load_cache():
         return
 
     global _subjectid; global _nb_catimgs; global _categories;
-    global _random_seed; global _use_random_seed;
+    global _random_seed; global _use_seed;
     global _red_filter; global _cyan_filter;
 
     cache = _read_cache()
@@ -228,8 +229,11 @@ def _load_cache():
     _red_filter = cache[_CacheKeys.REDFILTER]
     _cyan_filter = cache[_CacheKeys.CYANFILTER]
     _random_seed = int(cache[_CacheKeys.RANDSEED])
-    _use_random_seed = bool(cache[_CacheKeys.USERANDSEED])
+    _use_seed = bool(cache[_CacheKeys.USERANDSEED])
 
+
+# other utilities
+ 
 def _verify_files() -> None:
     missing_files = False
     
@@ -264,22 +268,33 @@ def _verify_files() -> None:
     logger.info("[INFO]: Stimuli data successfully generated!")
 
 
-def get_images() -> Dict[str, Path]:
-    images = defaultdict(list)
+def get_stimuli() -> Dict[str, Path]:
+    stimuli_dict = defaultdict(list)
 
     subject_modalities = [m for m in Modalities]
     n = _subjectid % NB_MODALITIES
     subject_modalities = subject_modalities[-n:] + subject_modalities[:-n]
 
     for img_path in _TARGET_PATH.rglob("*.jpg"):
-        images[img_path.parent.name].append(
+        stimuli_dict[img_path.parent.name].append(
             (
                 img_path, 
-                subject_modalities[len(images[img_path.parent.name])%NB_MODALITIES]
+                subject_modalities[len(stimuli_dict[img_path.parent.name])%NB_MODALITIES]
             )
         )
+
+    # flatten result to list
+    stimuli_list = list(chain.from_iterable(stimuli_dict.values()))
+
+    return stimuli_list
+
+def get_modality_path(target_path:Path, modality:Modalities) -> Path:
+    if modality == Modalities.HIGH:
+        return _HIGH_MODALITY_PATH / "/".join(target_path.parts[-3:])
+    elif modality == Modalities.MEDIUM:
+        return _MEDIUM_MODALITY_PATH / "/".join(target_path.parts[-3:])
     
-    return images
+    
 
 # VISUAL =======================================================================
 
@@ -290,6 +305,16 @@ def get_fixation_cross(win=None) -> visual.ShapeStim:
         win, vertices='cross', 
         size=(win.size[0]*.01, win.size[1]*.01*(win.size[0]/win.size[1])), 
         fillColor='black', lineColor=None)
+
+def get_stimulus_frame(win=None) -> visual.Circle:
+    if win is None: win=WIN
+    return visual.Circle(win, size=win.size[0]*.5, color="gray")
+
+def get_stimulus_aperture(win=None) -> visual.Aperture:
+    if win is None: win=WIN
+    aperture = visual.Aperture(win, size=win.size[0]*.4, shape='circle') 
+    aperture.enabled = False
+    return aperture
 
 def get_mondrian_pattern(
         nb_rectangles, 
@@ -338,7 +363,7 @@ def get_mondrian_pattern(
 
 # FLOW =========================================================================
 
-def show_instructions(win=None):
+def show_instructions(win:visual.Window=None):
     if win is None: win=WIN
 
     instruction_text = visual.TextStim(
@@ -371,88 +396,165 @@ def show_instructions(win=None):
 
     event.waitKeys(keyList=['space'])
 
+def run_delay(win:visual.Window, duration:float, 
+              background:list=[], foreground:list=[]
+):
+    # draw background
+    for stim in background: stim.draw()
+    # draw foreground
+    for stim in foreground: stim.draw()
+    # update screen
+    win.flip()
+    # delay for duration seconds
+    core.wait(duration)
 
-# MAIN EXECUTION ===============================================================
+def run_cue_presentation(win:visual.Window, cue_path:Path, modality:Modalities, 
+                         duration:float, aperture:visual.Aperture, 
+                         background:list=[], foreground:list=[]
+):
+    # draw background
+    for stim in background: stim.draw()
 
-def run_cycle():
-    pass
+    if modality in [Modalities.HIGH, Modalities.MEDIUM]:
+        cue = visual.ImageStim(win, cue_path, size=win.size[0]*.4)
+
+    aperture.enabled = True
+    cue.draw()
+    aperture.enabled = False
+
+    # draw foreground
+    for stim in foreground: stim.draw()
+    # update screen
+    win.flip()
+    # delay for duration seconds
+    core.wait(duration)
+
+
+
+def run_stimulus(
+        win:visual.Window, target_path:Path, duration:float, 
+        flash_interval:float, aperture:visual.Aperture, 
+        background:list=[], foreground:list=[]
+):
+    response_time = None
+    
+    opacity = 1
+
+    clock = core.Clock()
+    start_time = clock.getTime()
+
+    while clock.getTime() - start_time < duration:
+        current_time = clock.getTime()
+
+        # update opacity (linearly decreases over time)
+        opacity = (current_time / duration)
+
+        # draw background
+        for stim in background: stim.draw()
+    
+        # draw CFS stimulus inside circular frame
+        aperture.enabled = True
+
+        target = visual.ImageStim(
+            win=win,
+            image=target_path,
+            size=win.size[0]*.4
+        )
+        target.setColor(_red_filter)
+        target.opacity = opacity
+        target.draw()
+        
+        mask = get_mondrian_pattern(200, (.7,.7), color=_cyan_filter, max_opacity=1-opacity)
+        mask.draw()
+        
+        aperture.enabled = False
+
+        # draw foreground
+        for stim in foreground: stim.draw()
+        # update screen
+        win.flip()
+
+        # check for key press during the loop
+        keys = event.getKeys(timeStamped=clock)  # Capture key presses with timestamps
+        if keys:
+            response_time = keys[0][1]  # Capture the time at which the key was pressed
+            print(f"Response Time: {response_time:.4f} seconds.")
+            break
+        
+        elapsed_time = clock.getTime() - current_time  # Time since last frame update
+        core.wait(flash_interval - elapsed_time)
+    
+    if response_time is None: print(f"No response.")
+    return response_time
+
 
 def run_trial():
-    background = get_mondrian_pattern(200)
+    if _use_seed:
+        np.random.seed(_random_seed)
+    else:
+        np.random.seed(int(time()))
+    
+    fixation_duration = .5 #s
+    cue_duration = 3 #s
+    delay_duration = .5 #s
+    stimulus_duration = 6 #s
 
-    stimuli = get_images()
+    flash_rate = 3  # Hz
+    flash_interval = 1.0 / flash_rate  # s
 
+    mondrian_background = get_mondrian_pattern(200)
+    stimulus_frame = visual.Circle(WIN, size=WIN.size[0]*.5, color="gray")  
+    fixation_cross = get_fixation_cross(WIN)
+    stimulus_aperture = get_stimulus_aperture(WIN)
 
+    stimuli = get_stimuli()
+    np.random.shuffle(stimuli)
+    print(stimuli)
+    
+    show_instructions()
+
+    # run a cycle for each stimulus
+    for stim in stimuli:
+        # run fixation phase
+        run_delay(
+            WIN, fixation_duration, 
+            background=[mondrian_background, stimulus_frame], 
+            foreground=[fixation_cross]
+        )
+        # run cue presentation phase
+        run_cue_presentation(WIN, get_modality_path(stim[0], stim[1]), stim[1], 
+                             cue_duration, stimulus_aperture, 
+                             background=[mondrian_background, stimulus_frame], 
+                             foreground=[fixation_cross]
+        )
+        # run stimulus presentation phase
+        run_stimulus(WIN, stim[0], stimulus_duration, 
+                     flash_interval, stimulus_aperture, 
+                     background=[mondrian_background, stimulus_frame], 
+                     foreground=[fixation_cross]
+        )
+        # run inter cycle delay (no fixation cross)
+        run_delay(WIN, delay_duration, 
+                  background=[mondrian_background, stimulus_frame], 
+                  foreground=[]
+        )
+
+    _update_cache(_CacheKeys.SUBJECTS, _subjectid+1)
+
+    WIN.close()
+    core.quit()
+
+# MAIN EXECUTION ===============================================================
 
 def main():
     _handle_args()
     _load_cache()
     _verify_files()
 
-    np.random.seed(_random_seed)
+    run_trial()
 
 
 if __name__ == "__main__":
-    _handle_args()
-    _load_cache()
-    _verify_files()
-
-    images = get_images()
-    show_instructions()
+    main()
     
-    background = get_mondrian_pattern(200)
-    stimulus_frame = visual.Circle(WIN, size=WIN.size[0]*.5, color="gray")
-    color_filter = np.array(_red_filter)
-
-
-    clock = core.Clock()
-    opacity = 1
-    start_time = clock.getTime()
-
-    flash_rate = 3  # Hz
-    flash_interval = 1.0 / flash_rate  # seconds
-    duration = 6  # seconds for the whole CFS trial
-
-    while clock.getTime() - start_time < duration:
-        # Get the current time
-        current_time = clock.getTime()
-
-        # Update opacity (smoothly decreases over time)
-        opacity = (current_time / duration)
-
-        background.draw()
-        stimulus_frame.draw()
-
-        aperture = visual.Aperture(WIN, size=WIN.size[0]*.4, shape='circle')
-        aperture.enabled = True
-        image = visual.ImageStim(
-            win=WIN,
-            image=images['bridge-68'][_subjectid%_nb_catimgs][0],
-            size=WIN.size[0]*.4
-        )
-        
-        image.setColor(color_filter)
-        image.opacity = opacity
-        image.draw()
-        mask = get_mondrian_pattern(200, (.7,.7), color=_cyan_filter, max_opacity=1-opacity)
-        mask.draw()
-        aperture.enabled = False
-
-        get_fixation_cross().draw()
-        WIN.flip()
-
-        # Check for vkey press during the loop
-        keys = event.getKeys(timeStamped=clock)  # Capture key presses with timestamps
-        if keys:
-            response_time = keys[0][1]  # Capture the time at which the key was pressed
-            print(f"Response Time: {response_time:.4f} seconds")
-            break
-        
-        elapsed_time = clock.getTime() - current_time  # Time since last frame update
-        core.wait(flash_interval - elapsed_time)
-
     
-    _update_cache(_CacheKeys.SUBJECTS, _subjectid+1)
-    
-    WIN.close()
-    core.quit()
